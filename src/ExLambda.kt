ee4cb5aa67885a43e7669bba6a772598d17b6899
kay <k.rahman0511@gmail.com>
2020-01-25 14:04:49 +0600

    Lamda

--- src/ExLambda.kt
+++ src/ExLambda.kt
@@ -1,3 +1,45 @@
 fun main(args:Array<String>){
-    print("Let's start kotlin")
+    println("Let's start kotlin")
+
+    val program = Program()
+     program.addTwoNumbers(5,7,object : MyInterface{
+        override fun execute(sum: Int) {
+            println(sum)
+        }
+    })
+
+
+    // {s-> println(s)}          //lamda expression
+
+    val aLambda :(Int) -> Unit = { s : Int -> println(s) }
+    val bLambda :(Int) -> Int = { s : Int -> 2 + 3 }
+    val cLambda :(Int) -> String = { s : Int -> "C LAMBDA" }
+
+    program.addTwoNumbers(5,9,aLambda)
+
+
+}
+
+
+class Program {
+
+    fun addTwoNumbers(a:Int,b:Int,action: (Int) -> Unit ){   //high level function with lambda as a parameter
+        val sum = a + b
+        action(sum)
+    }
+
+    fun addTwoNumbers(a:Int,b:Int,action:MyInterface){
+        val sum = a + b
+        action.execute(sum)
+
+    }
+
+    fun addTwoNumbers(a:Int,b:Int){
+        val sum = a+b
+        println(sum)
+    }
+}
+
+interface MyInterface{
+    fun execute(sum : Int)
 }
