b976a55feb3c4d0fb23d0ba30843d40f9e82778a
kay <k.rahman0511@gmail.com>
2020-02-03 23:58:35 +0600

    LambdaEx

--- src/ExLambda.kt
+++ src/ExLambda.kt
@@ -15,10 +15,19 @@ fun main(args:Array<String>){
     val bLambda :(Int) -> Int = { s : Int -> 2 + 3 }
     val cLambda :(Int) -> String = { s : Int -> "C LAMBDA" }
 
-    program.addTwoNumbers(5,9,aLambda)
+   // program.addTwoNumbers(5,9,aLambda)
 
+    val myLambdaFunction:(Int,Int) -> Int = {x,y -> x+y}
+    addTwoNumbers(3,8,myLambdaFunction)
 
 }
+
+fun addTwoNumbers(a:Int,b:Int,myFunc:(Int,Int)->Int){
+    var result  = myFunc(a,b)
+    println(result)
+}
+
+
 
 
 class Program {
